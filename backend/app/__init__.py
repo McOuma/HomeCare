30e9f86105d409a11e76964d04a3554d85d6782e
BonOuma <mcouma.mugah@gmail.com>
2024-03-24 11:23:50 +0300

    routes and test files added

---
+++ backend/app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.api_v1.routes import api
+
+db = SQLAlchemy()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Development')
+    db.init_app(app)
+    app.register_blueprint(api)
+    app.run()
---
+++ backend/app/api_v1/routes.py
@@ -0,0 +1,7 @@
+from app.api_v1 import api
+
+
+
+@api.route('/')
+def get_homepage():
+    return "<h1>We are Home</h1>"

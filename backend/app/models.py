8315e93f2689600ae1e19c8ae2f21cad1ec0cf83
BonOuma <mcouma.mugah@gmail.com>
2024-03-24 12:39:38 +0300

    exceptions

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,10 @@
+from app import db
+from flask import url_for
 from datetime import datetime
-
 from flask_login import UserMixin
 from sqlalchemy.ext.hybrid import hybrid_property
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app import db
 
 
 class User(db.Model, UserMixin):
@@ -25,3 +25,75 @@ class User(db.Model, UserMixin):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class Caregiver(User):
+    id  = db.Column(db.Integer(), foreign_key = True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(), nullable=False)
+    contact = db.Column(db.Integer(), nullable=False)
+    national_id = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Client(User):
+    id  = db.Column(db.Integer(), foreign_key = True)
+    first_name = db.Column(db.String(), nullable=False)
+    last_name = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(), nullable=False)
+    contact = db.Column(db.Integer(), nullable=False)
+    national_id = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Service(db.Model):
+    id  = db.Column(db.Integer(), foreign_key = True)
+    service_type = db.Column(db.String(), nullable=False)
+    service_price = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    duration = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Booking(db.Model):
+    id  = db.Column(db.Integer(), foreign_key = True)
+    client_name = db.Column(db.String(), nullable=False)
+    service = db.Column(db.String(), nullable=False)
+    booking_date = db.Column(db.String(), nullable=False)
+    status = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+
+class Booking_manager(db.Model):
+    id  = db.Column(db.Integer(), foreign_key = True)
+    client_name = db.Column(db.String(), nullable=False)
+    service_name = db.Column(db.String(), nullable=False)
+    booking_date = db.Column(db.String(), nullable=False)
+    status = db.Column(db.Integer(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class Review(db.Model):
+    id  = db.Column(db.Integer(), primary_key = True)
+    client_id = db.Column(db.Integer(), foreign_key=True)
+    service_id = db.Column(db.Integer(), foreign_key=True)
+    booking_id = db.Column(db.Integer(), foreign_key=True)
+    comment = db.Column(db.String(), nullable=False)
+    rating = db.Column(db.String(), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def get_url(self):
+        return url_for('api.get_review', id=self.id, _external=True)
+
+
+
+    def export_data(self):
+        return {
+            'self_url': self.get_url(),
+            'rating': self.rating,
+             'comment': self.comment,
+            'client_url': url_for('api.get_client', id=self.id,
+                                  _external=True)
+        }
